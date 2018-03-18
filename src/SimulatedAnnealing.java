9bf199f07e2172b1592330197f3b9abb39c6e029
dilloncharlesoleary <dillon651@gmail.com>
2018-03-17 22:02:22 -0500

    Added teh simulated annealing algo outline

--- src/SimulatedAnnealing.java
+++ src/SimulatedAnnealing.java
@@ -4,6 +4,14 @@ public class SimulatedAnnealing {
     public static void SimulatedAnnealingAlgo(BottleCapVisualizer visualizer, ArrayList<Position> positions, ArrayList<BottleTopType> capList)
     {
         ArrayList<BottleTopType> UnusedTops = new ArrayList<>(capList);
-
+        // Pick initial state, s
+        // k=0
+        // while k <k_max
+        //      T = temperature(k)
+        //      Randomly pick state t from neighbors of s
+        //      if f(t) > f(s) then s=t
+        //      else if (e^((f(t)-f(s))/T) > random() then s=t
+        //      k++
+        //  return s
     }
 }
